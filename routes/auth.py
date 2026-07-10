2a3410c4c7655ae8518363e1323caacca470c790
Eric Chhun <ericchhun@Erics-MacBook-Pro-6.local>
2026-07-09 17:14:36 -0700

    Add auth py

--- routes/auth.py
+++ routes/auth.py
@@ -8,7 +8,7 @@ auth_bp = Blueprint('auth', __name__)
 
 
 
-# Authentication routes
+# Authentication routes - needs to debug
 @auth_bp.route('/register', methods=['GET', 'POST'])
 def register():
     """ 
@@ -25,26 +25,35 @@ def register():
         email = request.form['email']
         password = request.form['password']
         
-        session['username'] = username # Store the username in the session for later use
-
+        # validate input if any field is empty
         if not username or not email or not password:
             return "All fields are required. Please fill out the form completely.", 400
         
-        # Check if user already exists
+        # check if the user already exists
         existing_user = User.query.filter((User.username == username) | (User.email == email)).first()
         if existing_user:
             return "Username or email already exists. Please choose another.", 400
         
-        # Create new user
-        hashed_password = generate_password_hash(password, method='sha256')
+        # hash the password
+        hashed_password = generate_password_hash(password)
+        # create a new user and save to the database
         new_user = User(username=username, email=email, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-        
-        return redirect(url_for('auth.login'))
-    
+        return redirect(url_for('auth.login')) # Redirect to the login page after successful registration
     return render_template('register.html')
 
+
+# route to diplay the user login on the home page
+@auth_bp.route('/profile')
+def profile():
+    user_id = session.get('user_id')
+    
+    if user_id:
+        user = User.query.get(user_id)
+        if user:
+            return render_template('profile.html', user=user) # Render the profile page with user information
+    return redirect(url_for('auth.login')) # Redirect to the login page if user is not logged in or user not found
 
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
