f5fd9ff8285357098fb411010afd06eda0eadc54
Eric Chhun <ericchhun@Erics-MacBook-Pro-6.local>
2026-07-19 20:47:02 -0700

    Add menu_data py

---
+++ menu_data.py
@@ -0,0 +1,79 @@
+""" 
+Database Seeding for menu Good days coffee shop
+
+"""
+menu_lunch = [
+    {
+        "name": "Turkey Sandwich",
+        "description": "A delicious turkey sandwich with lettuce, tomato, and mayo on whole wheat bread.",
+        "price": 7.99,
+        "category": "Lunch",
+        "image_filename": "lunch.png"
+    },
+    {
+        "name": "Veggie Wrap",
+        "description": "A healthy veggie wrap filled with fresh vegetables and hummus.",
+        "price": 6.99,
+        "category": "Lunch",
+        "image_filename": "lunch2.png"
+    },
+    {
+        "name": "Chicken Caesar Salad",
+        "description": "A classic Caesar salad topped with grilled chicken, croutons, and Parmesan cheese.",
+        "price": 8.99,
+        "category": "Lunch",
+        "image_filename": "lunch3.png"
+    },
+]
+
+
+
+menu_lemonade = [
+    {
+        "name": "classic lemonade",
+        "description": "A refreshing blend of freshly squeezed lemons, water, and a touch of sweetness.",
+        "price": 3.99,
+        "category": "Lemonade",
+        "image_filename": "lemonade1.png"
+    },
+    {
+        "name": "strawberry lemonade",
+        "description": "A delightful twist on classic lemonade, infused with the sweet and tangy flavor of ripe strawberries.",
+        "price": 4.49,
+        "category": "Lemonade",
+        "image_filename": "lemonade2.png"
+    },
+]
+
+
+menu_breakfast = [
+    
+    {
+        "name": "Hash Browns",
+        "description": "Crispy golden hash browns, perfect for a hearty breakfast.",
+        "price": 2.99,
+        "category": "Breakfast",
+        "image_filename": "breakfast1.png"
+    },
+    {
+        "name": "Sausage Biscuit",
+        "description": "A savory sausage patty nestled in a fluffy biscuit, ideal for a satisfying breakfast.",
+        "price": 3.49,
+        "category": "Breakfast",
+        "image_filename": "breakfast2.png"
+    },
+    {
+        "name": "Breakfast Burrito",
+        "description": "A delicious breakfast burrito filled with scrambled eggs, cheese, and your choice of bacon or sausage, wrapped in a warm tortilla.",
+        "price": 4.99,
+        "category": "Breakfast",
+        "image_filename": "breakfast3.png"
+    },
+    {
+        "name": "Bacon, sausage, and egg sandwich",
+        "description": "A hearty breakfast sandwich featuring crispy bacon, savory sausage, and a perfectly cooked egg, all nestled between a toasted bun.",
+        "price": 5.49,
+        "category": "Breakfast",
+        "image_filename": "breakfast4.png"
+    }
+]
